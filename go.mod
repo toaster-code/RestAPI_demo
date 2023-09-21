fe3d2d0567c9ca3ba06cbacf96e54ae618ca5a30
Fabio <fabio.manzoni@gmail.com>
2023-09-21 23:51:09 +0200

    Fixed mqtt_subscribe

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require github.com/gorilla/mux v1.8.0
 
 require (
 	github.com/eclipse/paho.mqtt.golang v1.4.3 // indirect
+	github.com/go-ini/ini v1.67.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	golang.org/x/net v0.8.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
--- mqtt_subscribe.go
+++ mqtt_subscribe.go
@@ -7,34 +7,99 @@ import (
 	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/go-ini/ini"
 )
 
 func onMessageReceived(client mqtt.Client, message mqtt.Message) {
 	// Print the message to the console with the time of the reception, and topic name:
 	fmt.Printf("[%s] %s\n", time.Now().Format(time.RFC3339), message.Payload())
-
-
 }
 
+func getConfigFromIniFile() (string, string, error) {
+	cfg, err := ini.Load("./config.ini")
+	if err != nil {
+		return "", "", err
+	} else {
+		if cfg.Section("DEFAULT").Key("server").String() != "" && cfg.Section("DEFAULT").Key("topic").String() != "" {
+			server := cfg.Section("DEFAULT").Key("server").String()
+			topic := cfg.Section("DEFAULT").Key("topic").String()
+			return server, topic, nil
+		} else {
+			return "", "", fmt.Errorf("missing settings in config file")
+		}
+	}
+}
+
+func getConfigFromEnvironment() (string, string, error) {
+	// Get the values of the environment variables
+	if os.Getenv("Mqtt_subscribe_server") == "" || os.Getenv("Mqtt_subscribe_topics") == "" {
+		return "", "", fmt.Errorf("missing environment variables Mqtt_subscribe_server or Mqtt_subscribe_topics")
+	} else {
+		server := os.Getenv("Mqtt_subscribe_server")
+		topic := os.Getenv("Mqtt_subscribe_topics")
+		return server, topic, nil
+	}
+}
+
+func getConfigFromArguments() (string, string, error) {
+	// Get the values of the command-line arguments
+	if len(os.Args) == 3 {
+		server := os.Args[1] // MQTT broker URI (e.g., "tcp://broker.hivemq.com:1883")
+		topic := os.Args[2]  // MQTT topic to subscribe to
+		return server, topic, nil
+	} else {
+		return "", "", fmt.Errorf("missing command-line arguments")
+	}
+}
+
+// getServerAndTopic returns the MQTT server and topic to subscribe to.
+// It first checks if the server and topic are provided as command line arguments.
+// If not, it checks if they are present in a config.ini file.
+// If not, it checks if they are set as environment variables.
+// If none of the above options are available, it returns an error.
+func getServerAndTopic() (string, string, error) {
+	var server, topic string
+	var err error
+
+	switch {
+	case len(os.Args) == 3:
+		server, topic, err = getConfigFromArguments()
+	case fileExists("config.ini"):
+		server, topic, err = getConfigFromIniFile()
+	case os.Getenv("Mqtt_subscribe_server") != "" && os.Getenv("Mqtt_subscribe_topics") != "":
+		server, topic, err = getConfigFromEnvironment()
+	default:
+		err = fmt.Errorf("no server or topic found")
+	}
+
+	return server, topic, err
+}
+
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+// main function starts the MQTT subscriber
 func main() {
 	// Inside main function
 	fmt.Println("Starting MQTT subscriber...")
 
-	// Check for the required command-line arguments
-	if len(os.Args) != 3 {
-		fmt.Println("Usage: mqtt_subscribe <broker_uri> <topic>")
+	// Get the MQTT broker URI and topic name
+	server, topic, err := getServerAndTopic()
+	if err != nil {
+		fmt.Printf("Error getting server and topic: %v\n", err)
 		os.Exit(1)
+	} else {
+		fmt.Printf("Connecting to %s and subscribing to topic '%s'\n", server, topic)
 	}
-
-	brokerURI := os.Args[1] // MQTT broker URI (e.g., "tcp://broker.hivemq.com:1883")
-	topic := os.Args[2]     // MQTT topic to subscribe to
-
-	// Print the MQTT broker URI and topic name
-	fmt.Printf("Connecting to %s and subscribing to topic '%s'\n", brokerURI, topic)
 
 	// Create an MQTT client options struct
 	opts := mqtt.NewClientOptions()
-	opts.AddBroker(brokerURI)
+	opts.AddBroker(server)
 
 	// Create an MQTT client instance
 	client := mqtt.NewClient(opts)
@@ -45,7 +110,7 @@ func main() {
 		os.Exit(1)
 	} else {
 		// Print a message indicating that we're connected with the MQTT broker and display the client ID and broker URI
-		fmt.Printf("Connected to MQTT broker with client ID '%s' and broker URI '%s'\n", opts.ClientID, brokerURI)
+		fmt.Printf("Connected to MQTT broker with client ID '%s' and broker URI '%s'\n", opts.ClientID, server)
 	}
 
 	// Subscribe to the specified MQTT topic and set the callback function
@@ -75,5 +140,4 @@ func main() {
 	client.Disconnect(250)
 
 	fmt.Println("MQTT disconnected")
-
 }
